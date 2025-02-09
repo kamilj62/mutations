4ac389193abffbf336a91fe94a172796898c0b1e
Joseph Kamil <kamilj@umich.edu>
2025-02-09 01:17:06 -0800

    changed .env

--- app/feed/error.js
+++ app/feed/error.js
@@ -1,10 +1,11 @@
-'use client';
+"use client";
 
-export default function FeedError() {
+export default function FeedError(error) {
   return (
     <>
       <h2>An error occurred!</h2>
       <p>Unfortunately, something went wrong. We're working on it!</p>
+      <p>{error.message}</p>
     </>
   );
 }
